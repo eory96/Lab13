bc841c0877749683e609b9dd0295ea8209d68613
eory96 <21500057@handong.edu>
2018-04-29 12:56:01 +0900

    add to index prob 5

--- src/main/java/edu/handong/csee/java/lab13/prob5/Equals.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Equals.java
@@ -1,20 +1,20 @@
 package edu.handong.csee.java.lab13.prob5;
 
-public class Equals {
-	public static boolean equals(int[][] m1, int[][] m2) {
-		int count = 0;
+public class Equals {//this class compare to m1-2d array and m2-2d array
+	public static boolean equals(int[][] m1, int[][] m2) {//this method don't have to initiate 
+		int count = 0;//variable for count number of same
 		if (m1.length != m2.length) // if the number of row is different, return false
-		return false;
+		return false;//return false
 		      
-		for (int i = 0; i < m1.length; i++) {
-		     for (int j = 0; j < m1[i].length; j++){
-		    	 if (m1[i][j] != m2[i][j])
-		                count++;
+		for (int i = 0; i < m1.length; i++) {//loop for comparing 2d-array
+		     for (int j = 0; j < m1[i].length; j++){//loop for comparing 2-d array
+		    	 if (m1[i][j] != m2[i][j])//if m1 and m2 is not same
+		                count++;//plus one to count
 		      }
 		}
-		if(count <= 3)
-			return true;
+		if(count <= 3)//if count is less than 3
+			return true;//return true
 		else
-			return false;
+			return false;//return false
 	}
 }
--- src/main/java/edu/handong/csee/java/lab13/prob5/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Main.java
@@ -2,50 +2,55 @@ package edu.handong.csee.java.lab13.prob5;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main {//this it main class
 
-	public static void main(String[] args) {
-		int n1, n2 = 0;
-		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Enter row size(maximum 5): ");
-		n1 = keyboard.nextInt();
-		System.out.print("Enter column size(maximum 5): ");
-		n2 = keyboard.nextInt();
-		int[][] m1 = new int[n1][n2];
-		System.out.print("Enter row size(maximum 5): ");
-		n1 = keyboard.nextInt();
-		System.out.print("Enter column size(maximum 5): ");
-		n2 = keyboard.nextInt();
-		int[][] m2 = new int[n1][n2];
-		System.out.print("Enter List1: ");
-		for (int i = 0; i < m1.length; i++)
-			for (int j = 0; j < m1[i].length; j++)
-				m1[i][j] = keyboard.nextInt();
-		    	System.out.print("Enter list2: ");
-		    	for (int i = 0; i < m2.length; i++){
-		    		for (int j = 0; j < m2[i].length; j++)
-		    			m2[i][j] = keyboard.nextInt();
-		    	}
-		    	for (int i = 0; i < m1.length; i++){
-		    		for (int j = 0; j < m1[i].length; j++)
-		    			System.out.print(m1[i][j] + " ");
-		    		System.out.println();
-		    	}
-		    	System.out.println();
-		    	for (int i = 0; i < m2.length; i++){
-		    		for (int j = 0; j < m2[i].length; j++)
-		    			System.out.print(m2[i][j] + " ");
-		    		System.out.println();
-		    	}
-		    	System.out.println();
-		                  
-		        
-		    	if (m1.equals(m2)) 
-		    		System.out.println("The two arrays are approximately identical.");
-		    	else 
-		    		System.out.println("The two arrays are not identical");
-		    	}
-
-}
+	public static void main(String[] args) {//this is main method
+		int n1, n2 = 0;//it will save row size, and column size
+		Scanner keyboard = new Scanner(System.in);//using for keyboard
+		System.out.print("Enter row size(maximum 5): ");//printout "Enter row size(maximum 5): "
+		n1 = keyboard.nextInt();//save row size to n1
+		System.out.print("Enter column size(maximum 5): ");//printout "Enter column size(maximum 5): "
+		n2 = keyboard.nextInt();//save column size to n2
+		int[][] m1 = new int[n1][n2];//make array (row size n1 column size n2)
+		System.out.print("Enter row size(maximum 5): ");//print out "Enter row size(maximum 5): "
+		n1 = keyboard.nextInt();//save row size to n1
+		System.out.print("Enter column size(maximum 5): ");//print out "Enter column size(maximum 5): "
+		n2 = keyboard.nextInt();//save column size to n2
+		int[][] m2 = new int[n1][n2];//make 2d array(row size n1, column size n2)
+		
+		System.out.print("Enter List1: ");//print out "Enter List1: " 
+		for (int i = 0; i < m1.length; i++) {//loop to fill out 2d array
+			for (int j = 0; j < m1[i].length; j++)//loop for fill out 2d array
+				m1[i][j] = keyboard.nextInt();//fill out 2d array
+		}
+		
+		System.out.print("Enter list2: ");//printout "Enter List2:"
+		for (int i = 0; i < m2.length; i++){//loop for fill out 2d array
+			for (int j = 0; j < m2[i].length; j++)//loop for fill out column 
+				m2[i][j] = keyboard.nextInt();//fill out 2d array
+		}
+		    	
+		for (int i = 0; i < m1.length; i++){//loop for print out 2d array
+			for (int j = 0; j < m1[i].length; j++)//loop for print out 2d array
+				System.out.print(m1[i][j] + " ");//print out 2d array
+			System.out.println();//print out enter
+		}
+		System.out.println();//print out enter
+		
+		for (int i = 0; i < m2.length; i++){//loop for print out 2d array
+			for (int j = 0; j < m2[i].length; j++)//loop for print out 2d array
+				System.out.print(m2[i][j] + " ");//print out 2d array
+			System.out.println();//print out enter
+		}
+		System.out.println();//print out enter
+		
+		
+		if (m1.equals(m2)) //compare m1 and m2 array
+			System.out.println("The two arrays are approximately identical.");//if m1 and m2 same printout "The two arrays are approximately identical."
+		else //m1 and m2 is not same
+			System.out.println("The two arrays are not identical");//printout "The two arrays are not identical"
+	}
+	
+}	
 
 
