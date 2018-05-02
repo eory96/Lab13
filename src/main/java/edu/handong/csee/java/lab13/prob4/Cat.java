66ffcefb13acfb5e5de25fa14d2d451ac3e474c4
eory96 <21500057@handong.edu>
2018-05-02 22:26:42 +0900

    final commit

--- src/main/java/edu/handong/csee/java/lab13/prob4/Cat.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Cat.java
@@ -2,6 +2,6 @@ package edu.handong.csee.java.lab13.prob4;//prob4 package
 
 public class Cat extends Animal implements Pet{//it's parent class Animal and pet inherit Animal
 	public String whatEatTheFood() {//return type string method
-		return "(Cat!)"+"Fish";//return "(Dog!)"+"Sausage"
+		return "(Cat!)"+"Fish";//return "(Cat!)"+"Fish"
 	}
 }
